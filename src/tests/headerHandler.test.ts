0794845a767e5ea5aaf0739c4a7a70d52f38a479
incendiarybean <ben.weare12@gmail.com>
2022-10-17 16:33:53 +0100

    Updated tests to use mapping

--- src/tests/headerHandler.test.ts
+++ src/tests/headerHandler.test.ts
@@ -1,12 +1,12 @@
 import request from "supertest";
-import { HTTPServer, app } from "../server";
+import { HTTPServer, app } from "@server";
 
 describe("Server should redirect to HTTPS when HTTP is used", () => {
-  test("Status code matches the redirect", async () => {
-    const result = await request(app)
-      .get("/")
-      .set("x-forwarded-proto", "http://test.com");
-    HTTPServer.close();
-    expect(result.status).toBe(301);
-  });
+    test("Status code matches the redirect", async () => {
+        const result = await request(app)
+            .get("/")
+            .set("x-forwarded-proto", "http://test.com");
+        HTTPServer.close();
+        expect(result.status).toBe(301);
+    });
 });
